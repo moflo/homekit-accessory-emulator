bf505da7c3e727b4eadb5657e75501540610e17d
mobileflowllc <fieldforceapp@gmail.com>
2017-06-09 10:23:18 -0700

    Initial Commit

---
+++ homekit-emulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  homekit-emulator
+//
+//  Created by d. nye on 6/9/17.
+//  Copyright © 2017 Mobile Flow LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
